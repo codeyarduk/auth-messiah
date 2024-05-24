6389eb14fc2ea68dccd34bd278f3c0fba364eb87
Joey Jooste <joeyjjooste@gmail.com>
2024-05-24 18:56:13 +0200

    Add Zod Validation through a POST endpoint

--- src/routes/publicRoutes.ts
+++ src/routes/publicRoutes.ts
@@ -1,4 +1,6 @@
 import { Hono } from 'hono';
+import { z } from 'zod';
+import { zValidator } from '@hono/zod-validator';
 
 const publicRoutes = new Hono();
 
@@ -6,8 +8,13 @@ publicRoutes.get('/login', (c) => {
 	return c.text('Login Route');
 });
 
-publicRoutes.get('/register', (c) => {
-	return c.text('Register Route');
-});
+publicRoutes.post(
+	'/register',
+	zValidator('json', z.object({ email: z.string().min(1).email(), password: z.string().min(1).max(255) })),
+	async (c) => {
+		const validated = await c.req.json();
+		return c.json(`This is the email: ${validated.email} /n/n This is the password: ${validated.password}`);
+	},
+);
 
 export { publicRoutes };
