bd558a901c6590ccfa0421bec4c8594f336ec68a
davidwrossiter <davewrossiter@gmail.com>
2024-05-29 20:17:57 +0100

    add JSX with integration

--- src/views/components/Input.tsx
+++ src/views/components/Input.tsx
@@ -2,19 +2,19 @@ type InputProps = {
 	label: string;
 	name: string;
 	placeholder: string;
-	required: boolean;
+
 	type: 'text' | 'textarea' | 'password';
 	hasBottomMargin?: boolean;
 };
 
-export default function Input({ label, name, placeholder, required, type, hasBottomMargin = false }: InputProps) {
+export default function Input({ label, name, placeholder, type, hasBottomMargin = false }: InputProps) {
 	return (
-		<div class={hasBottomMargin ? 'mb-6' : undefined}>
-			<label for={name} class="block mt-4 text-sm font-medium text-gray-900 mb-2">
+		<div class={hasBottomMargin ? 'mb-2' : undefined}>
+			{/* <label for={name} class="block mt-4 text-sm font-medium text-gray-900 mb-2">
 				{label}
-			</label>
+			</label> */}
 
-			<input type={type} name={name} class="border rounded-lg p-3 pl-4 mb-2 w-full" placeholder={placeholder} required={required} />
+			<input type={type} name={name} class="text-sm border rounded-lg p-2.5 pl-4 mb-2 w-full" placeholder={placeholder} />
 		</div>
 	);
 }
